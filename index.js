5a8c7aa869b6650766e6e804b72bd1249cc07c3f
shettycs <135249208+shettycs@users.noreply.github.com>
2024-08-11 00:52:14 +0530

    added-new-line

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-
+console.log('Express api poc')
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
