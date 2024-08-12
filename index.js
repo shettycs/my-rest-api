0955cce1e5f7cece697f1d2c38ebf1fd18a218ab
shettycs <135249208+shettycs@users.noreply.github.com>
2024-08-12 21:39:12 +0530

    added-api-line

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello, World!' });
   });
 
+let todos = [];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/api/todos', (req, res) => {
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
